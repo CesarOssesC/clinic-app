56d21a829df038f99f5a2220d8e852f034f1703f
CesarOsses <cesar.osses@gmail.com>
2024-04-08 10:05:35 -0400

    modificando consultexamrepo

--- src/main/java/com/cursospring/repo/IGenericRepo.java
+++ src/main/java/com/cursospring/repo/IGenericRepo.java
@@ -1,9 +1,9 @@
 package com.cursospring.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.NoRepositoryBean;
 
-// @NoRepositoryBean
+@NoRepositoryBean
 public interface IGenericRepo<T, ID> extends JpaRepository<T, ID> {
     
 }
